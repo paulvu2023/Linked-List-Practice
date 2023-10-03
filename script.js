68e25fa2356b61669469839a435122ccd8d662a3
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2023-10-03 10:07:29 -0700

    Create prepend function for LinkedList class

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-class LinkedList {}
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  prepend(value) {
+    const nextNode = null;
+    if ((head = null)) {
+      tail = nextNode;
+    } else {
+      nextNode = head.nextNode;
+    }
+    const newNode = new Node(value, nextNode);
+    head = newNode;
+  }
+}
 
 class Node {
   constructor(value = null, nextNode = null) {
